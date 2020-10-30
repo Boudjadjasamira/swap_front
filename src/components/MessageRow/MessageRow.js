c6b4dfb5355e004bf08fca1f73bbf60df711bf29
Administrator <admin@example.com>
2020-10-30 10:20:15 +0100

    suppression des contacts dans le dashboard

--- src/components/MessageRow/MessageRow.js
+++ src/components/MessageRow/MessageRow.js
@@ -1,83 +1,114 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import $ from 'jquery';
 
 export default class MessageRow extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-          allContact: [],
+          nom: "",
+          prenom: "",
+          categorie: "",
+          message: "",
+          photo: "",
+          idContact: 0,
+          modalNameDelete: "",
+          supprimerLaLigne: "display:block"
         }
+        this.deleteContact = this.deleteContact.bind(this);
+    }
       
-    
-     
-    
-        //on recupere les messages 
-            axios.get(`http://localhost:8000/api/contacts/` + this.props.idContact)
-            .then(res =>
-                this.setState({
-                  nom: res.data.nom,
-                  prenom: res.data.prenom,
-                  categorie: res.data.categorie,
-                  message: res.data.message,
-                  photo: res.data.photo
-                })
-            )
+    componentDidMount(){
+        axios.get(`http://localhost:8000/api/contacts/` + this.props.idContact)
+        .then(res =>
+            this.setState({
+              nom: res.data.nom,
+              prenom: res.data.prenom,
+              categorie: res.data.categorie,
+              message: res.data.message,
+              photo: res.data.photo,
+              idContact: this.props.idContact,
+              modalNameDelete: "deleteModel" + this.props.idContact
+            })
+        )
+    }
 
-      }
-
+    deleteContact(){
+        axios.delete('http://localhost:8000/api/contacts/' + this.state.idContact)
+        .then((res => 
+            this.setState({
+                supprimerLaLigne: "none"
+            })  
+        ))
+    }
+    
     render() {
         return (
-            
-                                    <tbody>
-                                        <tr>
-                                        <td>{this.state.nom}</td>
-                                        <td>{this.state.prenom}</td>
-                                        <td>{this.state.categorie}</td>
-                                        <td>{this.state.message}</td>
-                                        <td><img
-                            className="image-contact"
-                            src={"http://localhost:8000/uploads/contacts/" + this.state.photo}
-                            alt="imageContact"
-                            /></td>
-                                        <td>
-                                            <a
-                                            href="#editEmployeeModal"
-                                            className="edit"
-                                            data-toggle="modal"
-                                            >
-                                            <i
-                                                className="material-icons"
-                                                data-toggle="tooltip"
-                                                title="Répondre"
-                                            >
-                                                
-                                            </i>
-                                            </a>
-                                            <a
-                                            href="#deleteEmployeeModal"
-                                            className="delete"
-                                            data-toggle="modal"
-                                            >
-                                            <i
-                                                className="material-icons"
-                                                data-toggle="tooltip"
-                                                title="Supprimer"
-                                            >
-                                                
-                                            </i>
-                                            </a>
-                                        </td>
-                                        </tr>
-                                        </tbody>
-                                  
-                    
-        
- 
+            <tr style={{display: this.state.supprimerLaLigne}}>
+                <td>{this.state.nom}</td>
+                <td>{this.state.prenom}</td>
+                <td>{this.state.categorie}</td>
+                <td>{this.state.message}</td>
+                <td><img className="image-contact" src={"http://localhost:8000/uploads/contacts/" + this.state.photo} alt="imageContact" /></td>
+                <td>
+                    <a href="#editEmployeeModal" className="edit" data-toggle="modal" >
+                        <i className="material-icons" data-toggle="tooltip" title="Répondre"></i>Editer
+                    </a>
+                    <a href={"#" + this.state.modalNameDelete} className="delete" data-toggle="modal">
+                        <i className="material-icons" data-toggle="tooltip" title="Supprimer"></i>Supprimer
+                    </a>
+                </td>
+                
+                {/* MODAL EDIT */}
+                <div id="editEmployeeModal" className="modal fade">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h4 className="modal-title">Envoyer message</h4>
+                                <button type="button" className="close" data-dismiss="modal" aria-hidden="true">×</button>
+                            </div>
+                            <div className="modal-body">
+                                <div className="form-group">
+                                    <label>Pseudo</label>
+                                    <input type="text" className="form-control" required />
+                                </div>
+                                <div className="form-group">
+                                    <label>Email</label>
+                                    <input type="email" className="form-control" required />
+                                </div>
+                                <div className="form-group">
+                                    <label>Message</label>
+                                    <textarea className="form-control" required defaultValue={""} />
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <input type="button" className="btn btn-default" data-dismiss="modal" defaultValue="Cancel"/>
+                                <input type="submit" className="btn btn-info" defaultValue="Envoyer" />
+                            </div>           
+                        </div>
+                    </div>
+                </div>
 
-
-         )
-        }
-        
-         
+                {/* MODAL DELETE */}
+                <div id={this.state.modalNameDelete} className="modal fade">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h4 className="modal-title">Supprimer le message</h4>
+                                <button type="button" className="close" data-dismiss="modal" aria-hidden="true">×</button>
+                            </div>
+                            <div className="modal-body">
+                                <p>Etes-vous sur ? Cette action est irréversible !</p>
+                            </div>
+                            <div className="modal-footer">
+                                <input type="button" className="btn btn-default" data-dismiss="modal"  defaultValue="Annuler"/>
+                                <button className="btn btn-warning" data-dismiss="modal" onClick={this.deleteContact}>Supprimer</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </tr>
+        )
+    }    
 };
--- src/view/Dashboard/Dashboard.js
+++ src/view/Dashboard/Dashboard.js
@@ -4,176 +4,65 @@ import '../../css/styles.css';
 import axios from 'axios';
 import CardDashboard from '../../components/CardDashboard/CardDashboard';
 import MessageRow from '../../components/MessageRow/MessageRow';
-import Swal from 'sweetalert2';
 
 export default class Dashboard extends Component {
 
-    
     constructor(props) {
         super(props);
         this.state = {
           allContact: [],
         }
-       
-      }
+    }
       
-    
-      componentDidMount() {
+    componentDidMount() {
         document.title = "Dashboard";
-        
-    
         axios.get(`http://localhost:8000/api/contacts`)
-    .then(res => {
-      this.setState({ allContact: res.data['hydra:member'] });
-    })
-    
+        .then(res => {
+            this.setState({ allContact: res.data['hydra:member'] });
+        })
     } 
 
-    deleteMessage () {
-        axios.delete('http://localhost:8000/api/contacts/' + this.props.idContact)
-            .then(res => {
-                this.setState({ allContact: res.data['hydra:member'] });                
-            })
-            Swal.fire({
-                icon: 'error',
-                text: 'Message supprimé',
-                showConfirmButton: false,
-            })
-    }
-
-    
-    
-    
-    
-  render() {
-    return (
-
-<div className="body" id="bodyHome">
-
-            <CardDashboard nombreMessagesContact={this.state.allContact.length.toString()}> </CardDashboard>
-            <div className="container-fluid">
-                                <div className="table-responsive">
-                                <div className="table-wrapper">
-                                    <div className="table-title">
-                                    <div className="row">
-                                        <div className="col-sm-6">
-                                        <h2>
-                                            MESSAGES
-                                        </h2>
-                                        </div>
+    render() {
+        return (
+            <div className="body" id="bodyHome">
+                <CardDashboard nombreMessagesContact={this.state.allContact.length.toString()}> </CardDashboard>
+                <div className="container-fluid">
+                    <div className="table-responsive">
+                        <div className="table-wrapper">
+                            <div className="table-title">
+                                <div className="row">
+                                    <div className="col-sm-6">
+                                        <h2>MESSAGES</h2>
                                     </div>
-                                    </div>
-                                    <table className="table table-striped table-hover">
-             <thead>
-                                        <tr>
+                                </div>
+                            </div>
+                            <table className="table table-striped table-hover">
+                                <thead>
+                                    <tr>
                                         <th>Nom</th>
                                         <th>Prenom</th>
                                         <th>Catégorie</th>
                                         <th>Message</th>
                                         <th>Piece-jointe</th>
                                         <th>Options</th>
-                                        </tr>
-                                    </thead>
-                                    
-                                    
-                                    {this.state.allContact.map(e => (
-                                        <MessageRow idContact={e.id}></MessageRow>
-                                        ))}
-                                        </table>
-                                </div>
-                                </div>
-                                
-                            </div>
-                            
-                  
-                            
-                            {/* Edit Modal HTML */}
-                            <div id="editEmployeeModal" className="modal fade">
-                                <div className="modal-dialog">
-                                <div className="modal-content">
-                                    <form>
-                                    <div className="modal-header">
-                                        <h4 className="modal-title">Envoyer message</h4>
-                                        <button
-                                        type="button"
-                                        className="close"
-                                        data-dismiss="modal"
-                                        aria-hidden="true"
-                                        >
-                                        ×
-                                        </button>
-                                    </div>
-                                    <div className="modal-body">
-                                        <div className="form-group">
-                                        <label>Pseudo</label>
-                                        <input type="text" className="form-control" required />
-                                        </div>
-                                        <div className="form-group">
-                                        <label>Email</label>
-                                        <input type="email" className="form-control" required />
-                                        </div>
-                                        <div className="form-group">
-                                        <label>Message</label>
-                                        <textarea className="form-control" required defaultValue={""} />
-                                        </div>
-                                    </div>
-                                    <div className="modal-footer">
-                                        <input
-                                        type="button"
-                                        className="btn btn-default"
-                                        data-dismiss="modal"
-                                        defaultValue="Cancel"
-                                        />
-                                        <input type="submit" className="btn btn-info" defaultValue="Envoyer" />
-                                    </div>
-                                    </form>
-                                </div>
-                                </div>
-                            </div>
-                            {/* Delete Modal HTML */}
-                            <div id="deleteEmployeeModal" className="modal fade">
-                                <div className="modal-dialog">
-                                <div className="modal-content">
-                                    <form>
-                                    <div className="modal-header">
-                                        <h4 className="modal-title">Supprimer le message</h4>
-                                        <button
-                                        type="button"
-                                        className="close"
-                                        data-dismiss="modal"
-                                        aria-hidden="true"
-                                        >
-                                        ×
-                                        </button>
-                                    </div>
-                                    <div className="modal-body">
-                                        <p>Etes-vous sur ? Cette action est irréversible !</p>
-                                    </div>
-                                    <div className="modal-footer">
-                                        <input
-                                        type="button"
-                                        className="btn btn-default"
-                                        data-dismiss="modal"
-                                        defaultValue="Annuler"
-                                        />
-                                        <button className="btn btn-warning" onClick={this.deleteMessage()}>Supprimer</button>
-                                    </div>
-                                    </form>
-                                </div>
-                                </div>
-    
-  </div>
-  <br />
-  <div className="row">
-          <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-            <p className="h6">SWAP - 2020 Tous droits réservés</p>
-          </div>
-        </div>
-  </div>
-
-
-    
-);
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {this.state.allContact.map(e => (
+                                    <MessageRow idContact={e.id}></MessageRow>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>     
+                <br />
+                <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                        <p className="h6">SWAP - 2020 Tous droits réservés</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
-}
-
