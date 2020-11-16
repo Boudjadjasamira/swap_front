d9ee5811970cee951121d6edabc3bbbbd38760ec
Boudjadja Samira <boudjadjasamira@gmail.com>
2020-11-16 12:13:58 +0100

    Update CardDashboard

--- src/view/Dashboard/Avis.js
+++ src/view/Dashboard/Avis.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/styles.css';
 import { Link } from 'react-router-dom';
+import CardDashboard from '../../components/CardDashboard/CardDashboard';
 
 export default class Avis extends Component {
     
@@ -11,63 +12,9 @@ export default class Avis extends Component {
 <div className="body" id="bodyHome">
   {/* Just an image */}
 
-                <div className="container-fluid">
-                    <nav className="navbar navbar-light bg-light nav justify-content-center">
-                    <Link to={process.env.PUBLIC_URL + "/"}>
-                        <img src="../assets/img/logo.png" alt="logo" width="80%" />
-                        </Link> 
-                    </nav>
-                    <div className="row">
-                        <div className="col-lg-3 col-sm-6">
-                        <div className="card-box bg-blue">
-                            <div className="inner">
-                            <h3>150 </h3>
-                            <p> MESSAGES </p>
-                            </div>
-                            <div className="icon">
-                            <i className="fa fa-comment" aria-hidden="true" />
-                            </div>
-                            <Link className="card-box-footer" to={process.env.PUBLIC_URL + "/Dashboard"}> View More <i className="fa fa-arrow-circle-right" /></Link>  
-                        </div>
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                        <div className="card-box bg-green">
-                            <div className="inner">
-                            <h3> 183 </h3>
-                            <p> MEMBRES</p>
-                            </div>
-                            <div className="icon">
-                            <i className="fa fa fa-users" aria-hidden="true" />
-                            </div>
-                            <Link className="card-box-footer" to={process.env.PUBLIC_URL + "/Membres"}> View More <i className="fa fa-arrow-circle-right" /></Link>  
-                        </div>
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                        <div className="card-box bg-orange">
-                            <div className="inner">
-                            <h3>650 </h3>
-                            <p> AVIS </p>
-                            </div>
-                            <div className="icon">
-                            <i className="fa fa-star" aria-hidden="true" />
-                            </div>
-                            <Link className="card-box-footer" to={process.env.PUBLIC_URL + "/Avis"}> View More <i className="fa fa-arrow-circle-right" /></Link>  
-                        </div>
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                        <div className="card-box bg-red">
-                            <div className="inner">
-                            <h3> 820 </h3>
-                            <p> ANNONCES </p>
-                            </div>
-                            <div className="icon">
-                            <i className="fa fa-list-alt" />
-                            </div>
-                            <Link className="card-box-footer" to={process.env.PUBLIC_URL + "/AdminAnnonces"}> View More <i className="fa fa-arrow-circle-right" /></Link>  
-                        </div>
-                        </div>
-</div>
- 
+                <div className="container-fluid">                   
+                    
+                <CardDashboard></CardDashboard>
                             <div className="container-fluid">
                                 <div className="table-responsive">
                                 <div className="table-wrapper">
