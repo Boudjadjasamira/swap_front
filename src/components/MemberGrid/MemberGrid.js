41ef64c96ef2e37970f50eb94472a9a64b09fc80
Administrator <admin@example.com>
2020-11-19 17:06:48 +0100

    optimize memberGrid.js

--- src/components/MemberGrid/MemberGrid.js
+++ src/components/MemberGrid/MemberGrid.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
 export default class MemberGrid extends Component {
-
-  componentDidMount(){
-   
-
-  }
-
 
   render() {
         return (
